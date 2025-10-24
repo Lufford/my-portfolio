d4722321c7d98bd5d4f937a0e5fdba14c24a33d8
Lufford <146119753+Lufford@users.noreply.github.com>
2025-10-23 22:23:00 -0400

    moved youtube api call to backend

--- my-project-server/server.js
+++ my-project-server/server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Videos = require("./Video");
 const app = express();
 const cors = require("cors");
+
 app.use(cors());
 app.use(express.json());
 
@@ -13,6 +14,24 @@ mongoose
     .connect(process.env.MONGODB_URI)
     .then(() => { console.log("db connected") })
     .catch((error) => { console.error(`error connecting to db. ${error}`) });
+
+//youtube data api fetch
+const fetchYTData = async (id) => {
+    try {
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id=${id}&key=${process.env.API_KEY}`);
+        const data = await response.json();
+        const videoData = (data.items[0]);
+        const vidTitle = videoData.snippet.title;
+        const vidDate = videoData.snippet.publishedAt;
+        const vidThumbnail = videoData.snippet.thumbnails.standard.url;
+        const vidChannel = videoData.snippet.channelTitle;
+        const vidInfo = ({ "title": vidTitle, "date": vidDate, "thumbnail": vidThumbnail, "channel": vidChannel, "id": id });
+        return vidInfo;
+    }
+    catch (e) {
+        console.log(e.message);
+    }
+}
 
 app.get("/video-project", async (req, res) => {
     try {
@@ -26,7 +45,8 @@ app.get("/video-project", async (req, res) => {
 app.post("/video-project", async (req, res) => {
 
     try {
-        const video = req.body;
+        const videoId = req.body;
+        const video = await fetchYTData(videoId.id);
         let checkVid = await Videos.exists(video);
 
         if (!checkVid) {
--- my-project/src/projects/project-components/video-project.jsx
+++ my-project/src/projects/project-components/video-project.jsx
@@ -2,43 +2,36 @@ import {useEffect, useState } from "react";
 
 
 export default function VideoProject() {
-    const key = import.meta.env.VITE_API_KEY;
     const [ytLink, setYTLink] = useState("");
-    const [vidInfo, setvidInfo] = useState({});
+    const [vidId, setVidId] = useState({});
     const [fetchedVids, setFetchedVids] = useState([]);
     const handleInput = (e) => {
         setYTLink(e.target.value);
+
     }
 
-    console.log(key);
-    const fetchYTData = async () => {
-        try {
-            const videoId = (ytLink.substring(32, 43)).toString();
-            //secure api
-            const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id=${videoId}&key=${key}`);
-            const data = await response.json();
-            const videoData = (data.items[0]);
-            const vidTitle = videoData.snippet.title;
-            const vidDate = videoData.snippet.publishedAt;
-            const vidThumbnail = videoData.snippet.thumbnails.standard.url;
-            const vidChannel = videoData.snippet.channelTitle;
-            setvidInfo({ "title": vidTitle, "date": vidDate, "thumbnail": vidThumbnail, "channel": vidChannel, "id": videoId });
-            //parse into json to store in db
+    const grabAndPost = async () => {
+        const videoId = (ytLink.substring(32, 43)).toString();
+        if (!videoId) {
+            window.alert("No ID found.");
         }
-        catch (e) {
-            console.log(e.message);
+        else {
+            await postData(videoId)
+            await fetchDBData();
+            setYTLink("");
+
         }
     }
 
-    const sendData = async () => {
+    const postData = async (id) => {
         try {
             const response = await fetch("http://localhost:3000/video-project", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(vidInfo)
+                body: JSON.stringify({"id": id})
             });
             const result = await response.json();
-            console.log("Server response " + result.message);
+            console.log("Server response: " + result.message);
 
         } catch (error) {
             console.error("error " + error.message)
@@ -52,19 +45,16 @@ export default function VideoProject() {
                 throw new Error("no response");
             }
             const data = await response.json();
+            data.reverse();
             setFetchedVids(data);
         } catch (error) {
             console.error("error " + error.message);
         }
     }
 
-    useEffect(() => {
-        sendData();
-        setYTLink("");
+    useEffect(()=>{
         fetchDBData();
-    }, [vidInfo])
-
-    // 
+    },[]);
 
     const listVideos = fetchedVids?.map(video =>
         <li key={video.id} className="m-1.5">
@@ -87,8 +77,8 @@ export default function VideoProject() {
             <div className="flex-nowrap bg-slate-600 place-content-center">
                 <h1>Recommend me a Video</h1>
                 <div className="flex-nowrap" >
-                    <input type="text" onChange={handleInput} value={ytLink} placeholder="Enter youtube link" className="border-1" />
-                    <button className="p-1.5 bg-slate-900 border-1 rounded" onClick={fetchYTData}>Ok</button>
+                    <input type="text" onChange={handleInput} value={ytLink} placeholder="Enter youtube link" className="border-1 p-1.5 m-0.5" />
+                    <button className="p-1.5 bg-slate-900 border-1 rounded" onClick={grabAndPost}>Ok</button>
                 </div>
 
             </div>
